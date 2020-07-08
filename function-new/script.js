9cd53d777f330172bf7949e6119fd69eae7fe80b
singhalharsh10 <hsinghal1_be18@thapar.edu>
2020-07-08 08:39:12 +0530

    classes without classes  added

---
+++ function-new/script.js
@@ -0,0 +1,10 @@
+// Making clases without using class
+// Funcion who are used to be as class must be start with capital and other should be small
+
+function Person(name, age) {
+    this.name = name
+    this.age = age
+}
+
+
+let p1 = new Person('Harsh', 12)
